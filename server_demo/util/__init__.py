cb29e3d415066342c5b339aefcb0b454910fbdc0
Skogen <muumlover@live.com>
2021-08-26 17:57:58 +0800

    fix js&css&fonts

---
+++ server_demo/util/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@Time    : 2020/1/8 15:51
+@Author  : Skogen
+@Email   : muumlover@live.com
+@Blog    : https://blog.ronpy.com
+@Project : zeronas
+@FileName: __init__.py
+@Software: PyCharm
+@license : (C) Copyright 2019 by Sam Wang. All rights reserved.
+@Desc    : 
+    
+"""
