2b95961a461f70da7aeb603902fe248e2d7f9103
Skogen <muumlover@live.com>
2021-08-30 16:29:00 +0800

    drop console.debug

--- web/vue.config.js
+++ web/vue.config.js
@@ -8,5 +8,16 @@ module.exports = {
                 args[0].title = "FileManager";
                 return args;
             })
+        config.optimization
+            .minimizer('terser')
+            .tap(args => {
+                Object.assign(args[0].terserOptions.compress, { // 生产模式 console.log 去除
+                    // drop_console: true,
+                    pure_funcs: [
+                        'console.debug',
+                    ]
+                })
+                return args
+            })
     }
 }
